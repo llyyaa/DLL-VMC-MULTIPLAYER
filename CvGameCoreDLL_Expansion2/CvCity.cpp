9ce4f36fee240b206541d3f2dc3eb7bf17fc1cee
Qingyin <3138584199@qq.com>
2024-10-20 15:13:30 +0800

    format: EnableCrops && EnableArmee

--- CvGameCoreDLL_Expansion2/CvCity.cpp
+++ CvGameCoreDLL_Expansion2/CvCity.cpp
@@ -7781,8 +7781,8 @@ void CvCity::processBuilding(BuildingTypes eBuilding, int iChange, bool bFirst, 
 		ChangeNumNoNuclearWinterLocal(pBuildingInfo->IsNoNuclearWinterLocal() && pBuildingInfo->GetID() == GC.getInfoTypeForString("BUILDING_MEGACITY_PYRAMID") ? iChange : 0);
 #endif
 #if defined(MOD_TROOPS_AND_CROPS_FOR_SP)
-        ChangeNumEnableCrops(pBuildingInfo->IsEnableCrops() * iChange );
-		ChangeNumEnableArmee(pBuildingInfo->IsEnableArmee() * iChange );
+        ChangeNumEnableCrops(pBuildingInfo->IsEnableCrops() ? iChange : 0);
+		ChangeNumEnableArmee(pBuildingInfo->IsEnableArmee() ? iChange : 0);
 #endif
 #if defined(MOD_INTERNATIONAL_IMMIGRATION_FOR_SP)
 		ChangeNumAllScaleImmigrantIn(pBuildingInfo->CanAllScaleImmigrantIn() ? iChange : 0);
